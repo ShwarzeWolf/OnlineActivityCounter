b7cb8b5ffe00314570a155930a41b1a4c89dab08
ShwarzeWolf <31793871+ShwarzeWolf@users.noreply.github.com>
2019-03-04 22:28:41 +0300

    reading urls added

--- src/background.js
+++ src/background.js
@@ -1,7 +1,29 @@
-//function addUrlToList(){
-//}
+let URLList = [];
 
 chrome.tabs.onCreated.addListener(function(){
-    alert("hi");
+    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+        let currentUrl = {};
+
+        //here should be a function to proceed url to first letters
+        currentUrl.name = tabs[0].url;
+        currentUrl.time = new Date();
+
+        URLList.push(currentUrl);
+    });
 });
 
+/*
+chrome.tabs.onActiveChanged.addListener(function(){
+    // I don't really get what does this construction do
+    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+        let currentUrl = tabs[0].url;
+        //here should be a function to proceed url to first letters
+        //alert(url);
+    });
+});
+
+/*
+chrome.tabs.onUpdated.addListener(function(){
+    alert("hello");
+})
+*/
